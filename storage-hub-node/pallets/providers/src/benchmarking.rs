a6a750d6d200289e32376a1f1a1747ea25e65caf
TDemeco <tdemeco@itba.edu.ar>
2024-03-12 15:16:15 -0400

    refactor: :construction: minor benchmark refactor

--- storage-hub-node/pallets/providers/src/benchmarking.rs
+++ storage-hub-node/pallets/providers/src/benchmarking.rs
@@ -3,18 +3,22 @@
 use super::*;
 
 #[allow(unused)]
-use crate::Pallet as Template;
+use crate::Pallet as StorageProviders;
 use frame_benchmarking::{benchmarks, impl_benchmark_test_suite, whitelisted_caller};
 use frame_system::RawOrigin;
 
 benchmarks! {
-	do_something {
-		let s in 0 .. 100;
-		let caller: T::AccountId = whitelisted_caller();
-	}: _(RawOrigin::Signed(caller), s)
-	verify {
-		assert_eq!(Something::<T>::get(), Some(s));
-	}
+    do_something {
+        let s in 0 .. 100;
+        let caller: T::AccountId = whitelisted_caller();
+    }: _(RawOrigin::Signed(caller), s)
+    verify {
+        assert_eq!(Something::<T>::get(), Some(s));
+    }
 }
 
-impl_benchmark_test_suite!(Template, crate::mock::new_test_ext(), crate::mock::Test,);
+impl_benchmark_test_suite!(
+    StorageProviders,
+    crate::mock::new_test_ext(),
+    crate::mock::Test,
+);
