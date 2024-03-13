1b0d86a150f44b01cf70170dc9fcf97cb8ce863c
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-03-13 11:07:11 +0000

    fixes

--- test/runtime/sample.test.ts
+++ test/runtime/sample.test.ts
@@ -1,11 +1,11 @@
 import { expect, test, describe } from "bun:test";
 
 describe("Sample test suite", () => {
-	test("2 + 2", () => {
-		expect(2 + 2).toBe(4);
-	});
+  test("2 + 2", () => {
+    expect(2 + 2).toBe(4);
+  });
 
-	test("truthy", () => {
-		expect(1 === Number("1")).toBeTrue;
-	});
+  test("truthy", () => {
+    expect(1 === Number("1")).toBeTrue;
+  });
 });
