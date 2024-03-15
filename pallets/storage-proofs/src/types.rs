117f63d138f20f698bbba524290d7696d5f36f0c
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2024-03-15 11:26:27 -0300

    feat: 🚧 Setup structure for Storage Proofs pallet (#8)
    * chore: :see_no_evil: Ignore VS Code's `settings.json`
    
    * feat: :construction: Add barebones for storage proofs pallet
    
    * build: :heavy_plus_sign: Add storage proofs pallet as runtime dependency
    
    * docs: :bulb: Add `todo` to consider additional check in `challenge` extrinsic
    
    * fix: :rotating_light: Fix cargo fmt issues
    
    * fix: :rotating_light: Fix clippy errors
    
    * feat: :construction: Add `LastCheckpointBlock` storage value to pallet
    
    * refactor: :fire: Remove `benchmarking`, `tests` and `mock` files with no useful code yet, to fix clippy
    
    * fix: :rotating_light: Fix clippy warnings of unused variables and code
    
    * refactor: :coffin: Remove unused `#[pallet::hooks]`
    
    * fix: :adhesive_bandage: Unifying `FileKey` and `ForestRoot` types into `MerkleHash`

---
+++ pallets/storage-proofs/src/types.rs
@@ -0,0 +1,50 @@
+use codec::{Decode, Encode};
+use frame_support::traits::fungible;
+use scale_info::TypeInfo;
+
+#[derive(Debug, Clone, PartialEq, Decode, Encode, TypeInfo)]
+pub enum ProofRejectionReason {
+    /// The proof was rejected because the root does not match the
+    /// Merkle Patricia Forest root.
+    RootMismatch,
+    /// The proof was rejected because the previous and next existing
+    /// leaves to a challenge were not consecutive, i.e. there is at
+    /// least one more existing leaf in between.
+    NotConsecutiveLeaves,
+}
+
+// ****************************************************************************
+// ********************* Syntactic sugar for types ****************************
+// ****************************************************************************
+
+/// Syntactic sugar for the AccountId type used in the storage proofs pallet.
+pub type AccountIdFor<T> = <T as frame_system::Config>::AccountId;
+
+/// The type for keys that identify a file within a Merkle Patricia Forest.
+/// Syntactic sugar for the MerkleHash type used in the storage proofs pallet.
+pub type FileKeyFor<T> = <T as crate::Config>::MerkleHash;
+
+/// The type for a root of a Merkle Patricia Forest.
+/// Syntactic sugar for the MerkleHash type used in the storage proofs pallet.
+pub type ForestRootFor<T> = <T as crate::Config>::MerkleHash;
+
+/// Syntactic sugar for the MaxChallengesPerBlock type used in the storage proofs pallet.
+pub type MaxChallengesPerBlockFor<T> = <T as crate::Config>::MaxChallengesPerBlock;
+
+/// Syntactic sugar for the MaxSpsChallengedPerBlock type used in the storage proofs pallet.
+pub type MaxSpsChallengedPerBlockFor<T> = <T as crate::Config>::MaxSpsChallengedPerBlock;
+
+/// Syntactic sugar for the ChallengesQueueLength type used in the storage proofs pallet.
+pub type ChallengesQueueLengthFor<T> = <T as crate::Config>::ChallengesQueueLength;
+
+/// Syntactic sugar for the StorageProviders type used in the storage proofs pallet.
+pub type StorageProvidersFor<T> = <T as crate::Config>::StorageProviders;
+
+/// Syntactic sugar for the StorageProvider type used in the storage proofs pallet.
+pub type SpFor<T> =
+    <<T as crate::Config>::StorageProviders as crate::StorageProvidersInterface>::StorageProvider;
+
+/// Syntactic sugar for the type of Balance used in the NativeBalances pallet.
+pub type BalanceFor<T> = <<T as crate::Config>::NativeBalance as fungible::Inspect<
+    <T as frame_system::Config>::AccountId,
+>>::Balance;
---
+++ pallets/storage-proofs/src/utils.rs
@@ -0,0 +1,62 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+use frame_support::{ensure, pallet_prelude::DispatchResult};
+use frame_system::pallet_prelude::BlockNumberFor;
+use sp_trie::CompactProof;
+
+use crate::{
+    pallet,
+    types::{AccountIdFor, BalanceFor, FileKeyFor, StorageProvidersFor},
+    ChallengesQueue, Error, Pallet, StorageProvidersInterface,
+};
+
+impl<T> Pallet<T>
+where
+    T: pallet::Config,
+{
+    // TODO: Document.
+    pub fn do_challenge(who: &AccountIdFor<T>, file_key: &FileKeyFor<T>) -> DispatchResult {
+        // TODO: Add user payment for challenging.
+
+        // Get challenge to queue from storage.
+        let mut challenges_queue = ChallengesQueue::<T>::get();
+
+        // Check if challenge is already queued. If it is, just return.
+        if challenges_queue.contains(file_key) {
+            return Ok(());
+        }
+
+        // Add challenge to queue.
+        challenges_queue
+            .try_push(*file_key)
+            .map_err(|_| Error::<T>::ChallengesQueueOverflow)?;
+
+        Ok(())
+    }
+
+    // TODO: Document and add proper parameters.
+    #[allow(unused_variables)]
+    pub fn do_submit_proof(submitter: &AccountIdFor<T>, proof: &CompactProof) -> DispatchResult {
+        // Check if submitter is a registered Storage Provider.
+        ensure!(
+            StorageProvidersFor::<T>::is_sp(submitter.clone()),
+            Error::<T>::NotStorageProvider
+        );
+
+        // TODO
+        unimplemented!()
+    }
+
+    // TODO: Document and add proper parameters.
+    pub fn do_new_challenges_round() -> DispatchResult {
+        // TODO
+        unimplemented!()
+    }
+
+    // TODO: Document and add proper parameters.
+    #[allow(unused_variables)]
+    fn stake_to_challenge_period(stake: BalanceFor<T>) -> BlockNumberFor<T> {
+        // TODO
+        unimplemented!()
+    }
+}
