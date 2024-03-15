e46b934c8f8520d996d225682434c5e4ad0c64aa
TDemeco <tdemeco@itba.edu.ar>
2024-03-15 14:42:43 -0300

    style: :rotating_light: fix rust formatter warning

--- pallets/providers/src/tests.rs
+++ pallets/providers/src/tests.rs
@@ -4,15 +4,18 @@ use sp_runtime::BoundedVec;
 
 #[test]
 fn it_does_something() {
-	new_test_ext().execute_with(|| {
-		// Dispatch a signed extrinsic.
-		assert_ok!(StorageProviders::msp_sign_up(
-			RuntimeOrigin::signed(1),
-			42,
-			BoundedVec::new(),
-			ValueProposition { data_limit: 10, protocols: BoundedVec::new() }
-		));
-		// Read pallet storage and assert an expected result.
-		assert_eq!(StorageProviders::get_total_capacity(&1), 42);
-	});
+    new_test_ext().execute_with(|| {
+        // Dispatch a signed extrinsic.
+        assert_ok!(StorageProviders::msp_sign_up(
+            RuntimeOrigin::signed(1),
+            42,
+            BoundedVec::new(),
+            ValueProposition {
+                data_limit: 10,
+                protocols: BoundedVec::new()
+            }
+        ));
+        // Read pallet storage and assert an expected result.
+        assert_eq!(StorageProviders::get_total_capacity(&1), 42);
+    });
 }
