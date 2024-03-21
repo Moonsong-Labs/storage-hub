5ba86e0e5759395c43c998424070d44641bea0ef
TDemeco <tdemeco@itba.edu.ar>
2024-03-21 17:29:55 -0300

    feat: :sparkles: add hold reason to runtime and new externalities builder

--- pallets/providers/src/mock.rs
+++ pallets/providers/src/mock.rs
@@ -1,4 +1,5 @@
-use frame_support::{derive_impl, parameter_types, traits::Everything};
+use crate as pallet_storage_providers;
+use frame_support::{construct_runtime, derive_impl, parameter_types, traits::Everything};
 use frame_system as system;
 use pallet_babe::{RandomnessFromOneEpochAgo, SameAuthoritiesForever};
 use sp_core::{ConstU128, ConstU32, Void, H256};
@@ -11,20 +12,21 @@ type Block = frame_system::mocking::MockBlock<Test>;
 type Balance = u128;
 
 // Configure a mock runtime to test the pallet.
-frame_support::construct_runtime!(
+construct_runtime!(
     pub enum Test
     {
-        System: frame_system::{Pallet, Call, Config<T>, Storage, Event<T>},
-        Balances: pallet_balances::{Pallet, Call, Storage, Config<T>, Event<T>},
-        BabeRandomness: pallet_babe::{Pallet, Call, Storage},
-        Timestamp: pallet_timestamp::{Pallet, Call, Storage, Inherent},
-        StorageProviders: crate::{Pallet, Call, Storage, Event<T>},
+        System: frame_system,
+        Balances: pallet_balances,
+        BabeRandomness: pallet_babe,
+        Timestamp: pallet_timestamp,
+        StorageProviders: pallet_storage_providers,
     }
 );
 
 parameter_types! {
     pub const BlockHashCount: u64 = 250;
     pub const SS58Prefix: u8 = 42;
+    pub const StorageProvidersHoldReason: RuntimeHoldReason = RuntimeHoldReason::StorageProviders(pallet_storage_providers::HoldReason::StorageProviderDeposit);
 }
 
 #[derive_impl(frame_system::config_preludes::TestDefaultConfig as frame_system::DefaultConfig)]
@@ -64,7 +66,7 @@ impl pallet_balances::Config for Test {
     type MaxLocks = ConstU32<10>;
     type MaxReserves = ();
     type ReserveIdentifier = [u8; 8];
-    type RuntimeHoldReason = ();
+    type RuntimeHoldReason = RuntimeHoldReason;
     type RuntimeFreezeReason = ();
     type FreezeIdentifier = ();
     type MaxHolds = ConstU32<10>;
@@ -93,9 +95,11 @@ impl pallet_timestamp::Config for Test {
 impl crate::Config for Test {
     type RuntimeEvent = RuntimeEvent;
     type NativeBalance = Balances;
+    type RuntimeHoldReason = RuntimeHoldReason;
     type StorageData = u32;
     type SpCount = u32;
     type HashId = H256;
+    type Hashing = BlakeTwo256;
     type MerklePatriciaRoot = H256;
     type ValuePropId = H256;
     type MaxMultiAddressSize = ConstU32<100>;
@@ -107,14 +111,40 @@ impl crate::Config for Test {
     type SpMinDeposit = ConstU128<10>;
     type SpMinCapacity = ConstU32<1>;
     type DepositPerData = ConstU128<2>;
-
     type ProvidersRandomness = RandomnessFromOneEpochAgo<Test>;
 }
 
 // Build genesis storage according to the mock runtime.
-pub fn new_test_ext() -> sp_io::TestExternalities {
+pub fn _new_test_ext() -> sp_io::TestExternalities {
     system::GenesisConfig::<Test>::default()
         .build_storage()
         .unwrap()
         .into()
+}
+
+// Externalities builder with predefined balances for accounts and starting at block number 1
+pub struct ExtBuilder;
+impl ExtBuilder {
+    pub fn build() -> sp_io::TestExternalities {
+        let mut t = frame_system::GenesisConfig::<Test>::default()
+            .build_storage()
+            .unwrap();
+        pallet_balances::GenesisConfig::<Test> {
+            balances: vec![
+                (0, 5_000_000),       // Alice = 0
+                (1, 10_000_000),      // Bob = 1
+                (2, 20_000_000),      // Charlie = 2
+                (3, 30_000_000),      // David = 3
+                (4, 400_000_000),     // Eve = 4
+                (5, 5_000_000_000),   // Ferdie = 5
+                (6, 600_000_000_000), // George = 6
+            ],
+        }
+        .assimilate_storage(&mut t)
+        .unwrap();
+
+        let mut ext = sp_io::TestExternalities::new(t);
+        ext.execute_with(|| System::set_block_number(1));
+        ext
+    }
 }
