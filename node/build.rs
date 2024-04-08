e07d18938bba91f1beda6f0e79951ef715164f3a
Alexandru Murtaza <alexandru@moonsonglabs.com>
2024-04-08 15:25:12 +0300

    fix: revert proto changes and add file watcher in build.rs

--- node/build.rs
+++ node/build.rs
@@ -5,6 +5,11 @@ const PROTOS: &[&str] = &["src/services/file_transfer/schema/provider.v1.proto"]
 fn main() {
     generate_cargo_keys();
 
+    // Tell Cargo to rerun this build script whenever the proto files change.
+    PROTOS.iter().for_each(|proto| {
+        println!("cargo:rerun-if-changed={}", proto);
+    });
+
     rerun_if_git_head_changed();
 
     prost_build::compile_protos(PROTOS, &["src/services/file_transfer/schema"]).unwrap();
