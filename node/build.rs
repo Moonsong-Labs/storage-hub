a5b227a4dba8616fefebb08d8f761d4ceac1166d
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2024-04-09 04:37:06 -0300

    feat: ✨ Launch Storage Providers async tasks in Substrate node (#28)
    * WIP, provider requests protocol setup
    
    * request protocol type changes with proto
    
    * refactor: :truck: Restructure spawned networking task as service
    
    * fix: :fire: Remove unimplemented inherent code that was preventing the parachain from running
    
    * test: :white_check_mark: Add local testing instructions and configurations with pure zombienet
    
    * refactor: :truck: Make `service` in `node` use the new architecture with the `file_transfer` service
    
    * revert: :rewind: Remove `pure_zombie` zombienet config file
    
    * chore: :hocho: remove superseded bsp and msp crates
    
    * feat: :wrench: add infrastructure crate and Storage trait
    
    * feat: port Actor and EventBus infra. Update FileTransferService to use them
    
    * fix: Cargo.lock
    
    * style: :art: Format Cargo.toml files with zepter
    
    * refactor: :recycle: Make dependencies in `node` all workspace dependencies
    
    * fix FileTransferServive event loop stream merge
    
    * fix: revert proto changes and add file watcher in build.rs
    
    * feat: integrate events & add emit event mock example
    
    ---------
    
    Co-authored-by: Michael Assaf <michael.assaf.edge@gmail.com>
    Co-authored-by: Alexandru Murtaza <alexandru@moonsonglabs.com>

--- node/build.rs
+++ node/build.rs
@@ -1,7 +1,16 @@
 use substrate_build_script_utils::{generate_cargo_keys, rerun_if_git_head_changed};
 
+const PROTOS: &[&str] = &["src/services/file_transfer/schema/provider.v1.proto"];
+
 fn main() {
     generate_cargo_keys();
 
+    // Tell Cargo to rerun this build script whenever the proto files change.
+    PROTOS.iter().for_each(|proto| {
+        println!("cargo:rerun-if-changed={}", proto);
+    });
+
     rerun_if_git_head_changed();
+
+    prost_build::compile_protos(PROTOS, &["src/services/file_transfer/schema"]).unwrap();
 }
