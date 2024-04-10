cbe3deaf4a68e9693eb4e843cfcc02484c62d7e9
TDemeco <tdemeco@itba.edu.ar>
2024-04-10 10:34:20 -0300

    feat: :construction: add mocked randomness to runtime for testing

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -14,7 +14,7 @@ use pallet_proofs_dealer::{CompactProof, TrieVerifier};
 use polkadot_runtime_common::xcm_sender::NoPriceForMessageDelivery;
 use smallvec::smallvec;
 use sp_api::impl_runtime_apis;
-use sp_core::{crypto::KeyTypeId, Get, OpaqueMetadata};
+use sp_core::{blake2_256, crypto::KeyTypeId, Get, OpaqueMetadata, H256};
 use sp_runtime::{
     create_runtime_str, generic, impl_opaque_keys,
     traits::{BlakeTwo256, Block as BlockT, IdentifyAccount, Verify},
@@ -33,7 +33,10 @@ use frame_support::{
     dispatch::DispatchClass,
     genesis_builder_helper::{build_config, create_default_config},
     parameter_types,
-    traits::{ConstBool, ConstU128, ConstU32, ConstU64, ConstU8, EitherOfDiverse, TransformOrigin},
+    traits::{
+        ConstBool, ConstU128, ConstU32, ConstU64, ConstU8, EitherOfDiverse, Randomness,
+        TransformOrigin,
+    },
     weights::{
         constants::WEIGHT_REF_TIME_PER_SECOND, ConstantMultiplier, Weight, WeightToFeeCoefficient,
         WeightToFeeCoefficients, WeightToFeePolynomial,
@@ -492,6 +495,31 @@ impl pallet_collator_selection::Config for Runtime {
     type WeightInfo = ();
 }
 
+// We mock the Randomness trait to use a simple randomness function when testing the pallet
+const EPOCH_DURATION_IN_BLOCKS: BlockNumber = 10;
+const BLOCKS_BEFORE_RANDOMNESS_VALID: BlockNumber = 3;
+pub struct MockRandomness;
+impl Randomness<H256, BlockNumber> for MockRandomness {
+    fn random(subject: &[u8]) -> (H256, BlockNumber) {
+        // Simple randomness mock that changes each block but its randomness is only valid after 3 blocks
+
+        // Concatenate the subject with the block number to get a unique hash for each block
+        let subject_concat_block = [
+            subject,
+            &frame_system::Pallet::<Runtime>::block_number().to_le_bytes(),
+        ]
+        .concat();
+
+        let hashed_subject = blake2_256(&subject_concat_block);
+
+        (
+            H256::from_slice(&hashed_subject),
+            frame_system::Pallet::<Runtime>::block_number()
+                .saturating_sub(BLOCKS_BEFORE_RANDOMNESS_VALID),
+        )
+    }
+}
+
 impl pallet_storage_providers::Config for Runtime {
     type RuntimeEvent = RuntimeEvent;
     type NativeBalance = Balances;
@@ -510,7 +538,8 @@ impl pallet_storage_providers::Config for Runtime {
     type DepositPerData = ConstU128<2>;
     type RuntimeHoldReason = RuntimeHoldReason;
     type Subscribers = FileSystem;
-    // TODO: type ProvidersRandomness = RandomnessFromOneEpochAgo<Runtime>;
+    type ProvidersRandomness = MockRandomness;
+    type MaxBlocksForRandomness = ConstU32<{ 2 * EPOCH_DURATION_IN_BLOCKS }>;
 }
 
 // TODO: remove this and replace with pallet treasury
