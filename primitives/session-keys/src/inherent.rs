303574bb13cf289a77ea293b5de7dc482e136107
Tobi Demeco <50408393+TDemeco@users.noreply.github.com>
2024-04-19 15:50:51 -0300

    feat: :sparkles: add pallet `randomness` to provide randomness for the runtime (#38)
    * feat: :sparkles: add extrinsics and storage for the new sign up process
    
    To have randomness in the SP ID generation we are introducing a new two-step sign up process: first, the user has to request to sign up as a SP (MSP or BSP) and afterwards, when the randomness requested is valid, it has to confirm the sign up.
    In this commit we:
    - Change the sign up extrinsics to accommodate for this new two-step process, adding two new ones: confirm_sign_up and cancel_sign_up
    - Add a new storage map that holds the users' sign up requests
    - Add a getter from that storage that returns a request
    - Add the events and errors related to this new process
    - Add a new type, StorageProvider, which is an enum that can have either the structure of the metadata of a MSP or of a BSP
    
    * feat: :sparkles: update mock runtime and add mocked randomness to it
    
    * feat: :sparkles: implement two-step sign up process
    
    * test: :recycle: add new tests + revamp old test suite
    
    * test: :recycle: organice tests by category
    
    * style: :art: move Subscribers type above constants
    
    * fix: :truck: update tests with new name of providers interface
    
    * fix: :bug: update mock runtimes with new `providers` config
    
    * fix: :white_check_mark: fix tests from the `file-system` pallet
    
    * feat: :construction: add mocked randomness to runtime for testing
    
    * fix: :bug: update `file-system` tests to use new helper function
    
    * fix: :bug: update runtime config to new structure
    
    * style: :rotating_light: fix formatting with `cargo fmt`
    
    * refactor: :art: change `confirm_sign_up` signature and add dispatch logic
    
    * fix: :bug: fix tests to use new `confirm_sign_up` logic
    
    * refactor: :zap: move check in `do_xsp_sign_up` to the top for quicker exit
    
    * refactor: :art: remove unnecesary intermediate variable when updating network's total capacity
    
    * fix: :bug: add checks for pending sign up requests
    
    * test: :white_check_mark: add tests for pending sign up requests check
    
    * refactor: :art: move testing function to `mock.rs`
    
    * docs: :memo: fix docs
    
    * test: :white_check_mark: add more tests for pending sign up request check
    
    * feat: :sparkles: add rough randomness pallet impl
    
    * fix: :construction: include pallet randomness in runtime's Cargo.toml
    
    * feat: :sparkles: add `Randomness` pallet to runtime
    
    * refactor: :fire: remove unused code
    
    * refactor: :recycle: change event fields and logic + hash randomness subject
    
    * test: :white_check_mark: add inherent test
    
    * style: :rotating_light: fix fmt error
    
    * fix: :bug: fix wrong merge from main
    
    * docs: :memo: updated docs
    
    * feat: :sparkles: improve mock and add new test
    
    * feat: :construction: adapt runtime to use relay chain constants
    
    * fix: :art: amend review
    
    clean up code, add on_initialize hook, change when storage is updated and remove old license comments
    
    * fix: :bug: fix Cargo.lock issue
    
    * fix: :adhesive_bandage: Include read op in weight calculation of `on_initialize` hook
    
    ---------
    
    Co-authored-by: Facundo Farall <37149322+ffarall@users.noreply.github.com>

---
+++ primitives/session-keys/src/inherent.rs
@@ -0,0 +1,61 @@
+use codec::{Decode, Encode};
+use sp_inherents::{Error, InherentData, InherentIdentifier, IsFatalError};
+use sp_runtime::RuntimeString;
+
+#[derive(Encode)]
+#[cfg_attr(feature = "std", derive(Debug, Decode))]
+pub enum InherentError {
+    Other(RuntimeString),
+}
+
+impl IsFatalError for InherentError {
+    fn is_fatal_error(&self) -> bool {
+        match *self {
+            InherentError::Other(_) => true,
+        }
+    }
+}
+
+impl InherentError {
+    /// Try to create an instance ouf of the given identifier and data.
+    #[cfg(feature = "std")]
+    pub fn try_from(id: &InherentIdentifier, data: &[u8]) -> Option<Self> {
+        if id == &INHERENT_IDENTIFIER {
+            <InherentError as codec::Decode>::decode(&mut &*data).ok()
+        } else {
+            None
+        }
+    }
+}
+
+/// The InherentIdentifier to set the babe randomness results
+pub const INHERENT_IDENTIFIER: InherentIdentifier = *b"baberand";
+
+/// A bare minimum inherent data provider that provides no real data.
+/// The inherent is simply used as a way to kick off some computation
+/// until https://github.com/paritytech/substrate/pull/10128 lands.
+pub struct InherentDataProvider;
+
+#[cfg(feature = "std")]
+#[async_trait::async_trait]
+impl sp_inherents::InherentDataProvider for InherentDataProvider {
+    async fn provide_inherent_data(&self, inherent_data: &mut InherentData) -> Result<(), Error> {
+        inherent_data.put_data(INHERENT_IDENTIFIER, &())
+    }
+
+    async fn try_handle_error(
+        &self,
+        identifier: &InherentIdentifier,
+        _error: &[u8],
+    ) -> Option<Result<(), sp_inherents::Error>> {
+        // Don't process modules from other inherents
+        if *identifier != INHERENT_IDENTIFIER {
+            return None;
+        }
+
+        // All errors with the randomness inherent are fatal
+        Some(Err(Error::Application(Box::from(String::from(
+            "Error processing dummy randomness inherent",
+        )))))
+    }
+}
