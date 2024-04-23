5190f6d0e71b010225b7ef823419edf9ead5a8b6
a-moreira <afm2@duck.com>
2024-04-23 20:56:36 +0000

    chore: lint

--- node/src/services/file_transfer/commands.rs
+++ node/src/services/file_transfer/commands.rs
@@ -15,11 +15,11 @@ pub trait FileTransferServiceInterface {
 }
 
 impl FileTransferServiceInterface for ActorHandle<FileTransferService> {
-    fn establish_connection(&self, multiaddresses: Vec<String>) -> anyhow::Result<()> {
+    fn establish_connection(&self, _multiaddresses: Vec<String>) -> anyhow::Result<()> {
         Ok(())
     }
 
-    fn send_file(&self, file: Vec<u8>) -> anyhow::Result<()> {
+    fn send_file(&self, _file: Vec<u8>) -> anyhow::Result<()> {
         Ok(())
     }
 }
