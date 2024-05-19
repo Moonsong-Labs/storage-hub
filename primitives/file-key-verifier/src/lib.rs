2b22d8bb85c3c5b16a9ee23a6e3c862cabdefee7
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2024-05-18 21:45:49 -0300

    feat: :sparkles: Implement `CommitmentVerifier` for `FileKeyVerifier`

--- primitives/file-key-verifier/src/lib.rs
+++ primitives/file-key-verifier/src/lib.rs
@@ -1,173 +1,144 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
+use codec::{Decode, Encode};
 use frame_support::sp_runtime::DispatchError;
-use sp_std::collections::btree_set::BTreeSet;
-use sp_std::vec::Vec;
+use num_bigint::BigUint;
+use scale_info::TypeInfo;
+use sp_core::Hasher;
+use sp_std::{collections::btree_set::BTreeSet, vec::Vec};
 use sp_trie::{CompactProof, TrieDBBuilder, TrieLayout};
 use storage_hub_traits::CommitmentVerifier;
-use trie_db::TrieIterator;
+use trie_db::Trie;
 
 #[cfg(test)]
 mod tests;
 
 /// A struct that implements the `CommitmentVerifier` trait, where the commitment
-/// is a Merkle Patricia Trie root hash.
-pub struct FileKeyVerifier<T: TrieLayout>
+/// is a Merkle Patricia Trie root hash and the response to a challenge is given
+/// by either the exact key or the next and previous keys in the trie.
+pub struct FileKeyVerifier<T: TrieLayout, const H_LENGTH: usize, const CHUNK_SIZE: u64>
 where
-    <T::Hash as sp_core::Hasher>::Out: TryFrom<Vec<u8>>,
+    <T::Hash as sp_core::Hasher>::Out: for<'a> TryFrom<&'a [u8; H_LENGTH]>,
 {
     pub _phantom: core::marker::PhantomData<T>,
 }
 
-/// Implement the `CommitmentVerifier` trait for the `TrieVerifier` struct.
-impl<T: TrieLayout> CommitmentVerifier for FileKeyVerifier<T>
+#[derive(Clone, Debug, PartialEq, Eq, TypeInfo, Encode, Decode)]
+pub struct FileKeyProof {
+    pub owner: Vec<u8>,
+    pub location: Vec<u8>,
+    pub size: u64,
+    pub fingerprint: Vec<u8>,
+    pub proof: CompactProof,
+}
+
+/// Implement the `CommitmentVerifier` trait for the `FileKeyVerifier` struct.
+impl<T: TrieLayout, const H_LENGTH: usize, const CHUNK_SIZE: u64> CommitmentVerifier
+    for FileKeyVerifier<T, H_LENGTH, CHUNK_SIZE>
 where
-    <T::Hash as sp_core::Hasher>::Out: TryFrom<Vec<u8>>,
+    <T::Hash as sp_core::Hasher>::Out: for<'a> TryFrom<&'a [u8; H_LENGTH]>,
 {
-    type Proof = CompactProof;
+    type Proof = FileKeyProof;
     type Key = <T::Hash as sp_core::Hasher>::Out;
 
     /// Verifies a proof against a root (i.e. commitment) and a set of challenges.
     ///
-    /// Assumes that the challenges are ordered in ascending numerical order, and not repeated.
+    /// Iterates over the challenges, computes the modulo of the challenged hashes with the number of chunks in the file,
+    /// and checks if the resulting leaf is in the proof.
     fn verify_proof(
-        root: &Self::Key,
+        expected_file_key: &Self::Key,
         challenges: &[Self::Key],
         proof: &Self::Proof,
     ) -> Result<Vec<Self::Key>, DispatchError> {
-        // This generates a partial trie based on the proof and checks that the root hash matches the `expected_root`.
-        let (memdb, root) = proof.to_memory_db(Some(root.into())).map_err(|_| {
-            "Failed to convert proof to memory DB, root doesn't match with expected."
-        })?;
-
-        let trie = TrieDBBuilder::<T>::new(&memdb, &root).build();
-
-        // `TrieDBKeyDoubleEndedIterator` should always yield a `None` or `Some(leaf)` with a value.
-        // `Some(leaf)` yields a `Result` and could therefore fail, so we still have to check it.
-        let mut trie_de_iter = trie
-            .into_double_ended_iter()
-            .map_err(|_| "Failed to create trie iterator.")?;
-
-        // Check if the iterator has at least one leaf.
-        if trie_de_iter.next().is_none() {
-            #[cfg(test)]
-            unreachable!(
-                "This should not happen. A trie with no leafs wouldn't be possible to create."
-            );
-
-            #[allow(unreachable_code)]
-            {
-                return Err("No leaves provided in proof.".into());
-            }
-        }
-
         // Check that `challenges` is not empty.
         if challenges.is_empty() {
             return Err("No challenges provided.".into());
         }
 
+        // Construct file key from the fields in the proof.
+        let file_key = T::Hash::hash(
+            &[
+                &proof.owner.encode(),
+                &proof.location.encode(),
+                &proof.size.encode(),
+                &proof.fingerprint.encode(),
+            ]
+            .into_iter()
+            .flatten()
+            .cloned()
+            .collect::<Vec<u8>>(),
+        );
+
+        // Check that the file key is equal to the root.
+        if &file_key != expected_file_key {
+            return Err(
+                "Root provided should be equal to the file key constructed from the proof.".into(),
+            );
+        };
+
+        // Convert the fingerprint from the proof to the output of the hasher.
+        let expected_root: &[u8; H_LENGTH] = proof
+            .fingerprint
+            .as_slice()
+            .try_into()
+            .map_err(|_| "Failed to convert fingerprint to a fixed size array.")?;
+        let expected_root: Self::Key = expected_root
+            .try_into()
+            .map_err(|_| "Failed to convert fingerprint to a hasher output.")?;
+
+        // This generates a partial trie based on the proof and checks that the root hash matches the `expected_root`.
+        let (memdb, root) = proof
+            .proof
+            .to_memory_db(Some(&expected_root))
+            .map_err(|_| {
+                "Failed to convert proof to memory DB, root doesn't match with expected."
+            })?;
+
+        let trie = TrieDBBuilder::<T>::new(&memdb, &root).build();
+
         // Initialise vector of proven keys. We use a `BTreeSet` to ensure that the keys are unique.
         let mut proven_keys = BTreeSet::new();
         let mut challenges_iter = challenges.iter();
 
-        // Iterate over the challenges and check if there is a pair of consecutive
-        // leaves that match the challenge, or an exact leaf that matches the challenge.
+        // Iterate over the challenges compute the modulo of the challenged hashes with the number of chunks in the file,
+        // and check if the resulting leaf is in the proof.
         while let Some(challenge) = challenges_iter.next() {
-            trie_de_iter
-                .seek(challenge.as_ref())
-                .map_err(|_| "Failed to seek challenged key.")?;
-
-            // Executing `next()` after `seek()` should yield the challenged leaf or the next leaf after it (which could be `None`).
-            let next_leaf = trie_de_iter
-                .next()
-                .transpose()
-                .map_err(|_| "Failed to get next leaf.")?;
-
-            // Executing `next_back()` after `seek()` should always yield `Some(leaf)` based on the double ended iterator behaviour.
-            let prev_leaf = trie_de_iter
-                .next_back()
-                .transpose()
-                .map_err(|_| "Failed to get previous leaf.")?;
-
-            // Check if there is a valid combination of leaves which validate the proof given the challenged key.
-            match (prev_leaf, next_leaf) {
-                // Scenario 1 (valid): `next_leaf` is the challenged leaf which is included in the proof.
-                // The challenge is the leaf itself (i.e. the challenge exists in the trie).
-                (_, Some((next_key, _))) if next_key == challenge.as_ref().to_vec() => {
-                    let next_key = next_key
-                        .try_into()
-                        .map_err(|_| "Failed to convert proven key.")?;
-
-                    proven_keys.insert(next_key);
-                    continue;
-                }
-                // Scenario 2 (valid): `prev_leaf` and `next_leaf` are consecutive leaves.
-                // The challenge is between the two leaves (i.e. the challenge exists in the trie).
-                (Some((prev_key, _)), Some((next_key, _)))
-                    if prev_key < challenge.as_ref().to_vec()
-                        && challenge.as_ref().to_vec() < next_key =>
-                {
-                    let prev_key = prev_key
-                        .try_into()
-                        .map_err(|_| "Failed to convert proven key.")?;
-
-                    proven_keys.insert(prev_key);
-
-                    let next_key = next_key
-                        .try_into()
-                        .map_err(|_| "Failed to convert proven key.")?;
-
-                    proven_keys.insert(next_key);
-
-                    continue;
-                }
-                // Scenario 3 (valid): `next_leaf` is the first leaf since the next previous leaf is `None`.
-                // The challenge is before the first leaf (i.e. the challenge does not exist in the trie).
-                (None, Some((next_key, _))) => {
-                    let next_key = next_key
-                        .try_into()
-                        .map_err(|_| "Failed to convert proven key.")?;
-
-                    proven_keys.insert(next_key);
-
-                    continue;
-                }
-                // Scenario 4 (valid): `prev_leaf` is the last leaf since `next_leaf` is `None`.
-                // The challenge is after the last leaf (i.e. the challenge does not exist in the trie).
-                (Some(prev_leaf), None) => {
-                    let prev_key = prev_leaf
-                        .0
-                        .try_into()
-                        .map_err(|_| "Failed to convert proven key.")?;
-
-                    proven_keys.insert(prev_key);
-
-                    continue;
-                }
-                // Invalid
-                (None, None) => {
-                    #[cfg(test)]
-                    unreachable!(
-                        "This should not happen. We check if the iterator has at least one leaf."
-                    );
-
-                    #[allow(unreachable_code)]
-                    {
-                        return Err("No leaves provided in proof.".into());
-                    }
-                }
-                _ => {
-                    #[cfg(test)]
-                    unreachable!(
-                        "This should not happen. We check if the iterator has at least one leaf."
-                    );
-
-                    #[allow(unreachable_code)]
-                    {
-                        return Err("Proof is invalid.".into());
-                    }
-                }
+            // Calculate the chunks of the file based on its size.
+            let mut chunks = proof.size / CHUNK_SIZE;
+            if proof.size % CHUNK_SIZE != 0 {
+                chunks += 1;
             }
+
+            // Calculate the modulo of the challenge with the number of chunks in the file.
+            // The challenge is a big endian 32 byte array.
+            let challenged_chunk = BigUint::from_bytes_be(challenge.as_ref()) % chunks;
+            let challenged_chunk: u64 = challenged_chunk.try_into().map_err(|_| {
+                "This is impossible. The modulo of a number with a u64 should always fit in a u64."
+            })?;
+
+            // Check that the chunk is in the proof.
+            let chunk = trie
+                .get(&challenged_chunk.to_be_bytes())
+                .map_err(|_| "The proof is invalid. The challenge does not exist in the trie.")?;
+
+            // The chunk should be Some(leaf) for the proof to be valid.
+            if chunk.is_none() {
+                return Err(
+                    "The proof is invalid. The challenge does not exist in the trie.".into(),
+                );
+            }
+
+            // Convert the challenged chunk to a key (now that we know it is a proven key).
+            let challenged_chunk_bytes = challenged_chunk.to_be_bytes();
+            let proven_key: &[u8; H_LENGTH] = challenged_chunk_bytes
+                .as_slice()
+                .try_into()
+                .map_err(|_| "Failed to convert a challenged chunk (u64) to a fixed size array.")?;
+            let proven_key: Self::Key = proven_key.try_into().map_err(|_| {
+                "Failed to convert a challenged chunk as an array of bytes to a key."
+            })?;
+            // Add the challenge to the proven keys.
+            proven_keys.insert(proven_key);
         }
 
         return Ok(Vec::from_iter(proven_keys));
