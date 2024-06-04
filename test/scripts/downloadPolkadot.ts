b75a2f7a0390d661349d4970649990baa141420d
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-06-04 08:22:55 +0100

    fix: :bug: Fix download polkadot script

--- test/scripts/downloadPolkadot.ts
+++ test/scripts/downloadPolkadot.ts
@@ -2,49 +2,60 @@ import fs from "node:fs";
 import path from "node:path";
 
 async function main() {
-  const binaries = ["polkadot", "polkadot-prepare-worker", "polkadot-execute-worker"];
-  const version = getVersionArg();
-  const ghRoot = "https://github.com/paritytech/polkadot-sdk/releases/download/";
-  const writeDir = path.join(process.cwd(), "./tmp/");
+	const binaries = [
+		"polkadot",
+		"polkadot-prepare-worker",
+		"polkadot-execute-worker",
+	];
+	const version = getVersionArg();
+	const ghRoot =
+		"https://github.com/paritytech/polkadot-sdk/releases/download/";
+	const writeDir = path.join(process.cwd(), "./tmp/");
 
-  try {
-    for (const binary of binaries) {
-      const writePath = path.join(writeDir, binary);
+	if (!fs.existsSync(writeDir)) {
+		fs.mkdirSync(writeDir, { recursive: true });
+	}
 
-      if (fs.existsSync(writePath)) {
-        console.log(`📂 ${binary} already exists at ${writePath}`);
-        continue;
-      }
+	try {
+		for (const binary of binaries) {
+			const writePath = path.join(writeDir, binary);
 
-      const downloadUri = `${ghRoot}polkadot-v${version}/${binary}`;
-      console.log(`💾 Downloading ${binary} from ${downloadUri}`);
-      const blob = await fetch(downloadUri);
-      const arrayBuffer = await blob.arrayBuffer();
-      const buffer = Buffer.from(arrayBuffer);
+			if (fs.existsSync(writePath)) {
+				console.log(`📂 ${binary} already exists at ${writePath}`);
+				continue;
+			}
 
-      fs.writeFileSync(writePath, buffer);
+			const downloadUri = `${ghRoot}polkadot-v${version}/${binary}`;
+			console.log(`💾 Downloading ${binary} from ${downloadUri}`);
+			const blob = await fetch(downloadUri);
+			const arrayBuffer = await blob.arrayBuffer();
+			const buffer = Buffer.from(arrayBuffer);
 
-      fs.chmod(writePath, 0o755, (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log(`File permissions set for ${writePath}`);
-      });
-    }
-  } catch (error) {
-    console.error(`❌ Failed to download Polkadot binaries: ${error}`);
-    process.exit(1);
-  }
+			fs.writeFileSync(writePath, buffer);
 
-  console.log(`✅ Polkadot downloader script completed at: ${process.cwd()}`);
+			fs.chmod(writePath, 0o755, (err) => {
+				if (err) {
+					throw err;
+				}
+				console.log(`File permissions set for ${writePath}`);
+			});
+		}
+	} catch (error) {
+		console.error(`❌ Failed to download Polkadot binaries: ${error}`);
+		process.exit(1);
+	}
+
+	console.log(`✅ Polkadot downloader script completed at: ${process.cwd()}`);
 }
 
 function getVersionArg() {
-  const args = process.argv.slice(2);
-  if (args.length === 0) {
-    throw new Error("No version provided. Usage: pnpm tsx scripts/downloadPolkadot.ts <version>");
-  }
-  return args[0];
+	const args = process.argv.slice(2);
+	if (args.length === 0) {
+		throw new Error(
+			"No version provided. Usage: pnpm tsx scripts/downloadPolkadot.ts <version>",
+		);
+	}
+	return args[0];
 }
 
 main();
