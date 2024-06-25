36e3fd8f697d9990531f99ec7c31e798287e43b5
Tim B <79199034+timbrinded@users.noreply.github.com>
2024-06-25 15:53:56 +0100

    feat: :zap: Add Dev Net scripts and automation (#95)
    * feat: :sparkles: initial go
    
    * feat: :zap: DevNet alive
    
    * feat: :sparkles: progress
    
    * feat: :zap: progress
    
    * feat: :zap: progress
    
    * fix: :bug: Error reporting
    
    * feat: :zap: Working Script
    
    * fix: :rotating_light: Lint
    
    * refactor
    
    * added extra test functions
    
    * added issue test
    
    * progress
    
    * fixed types woohoo
    
    * progress
    
    * feat: :sparkles: Fully working test suite, ci and docs
    
    magnus opus
    
    * style: :rotating_light: Lint
    
    * fix: :bug: Fix docker settings
    
    * fix: :bug: Added error logging
    
    * ci: :green_heart: CI Changes
    
    * fix container name
    
    * logging
    
    * fmt
    
    * Increase metadata timeouts
    
    * fmt
    
    * tidy ci
    
    * feat: :sparkles: Added new Ext success property on created blocks
    
    * feat: :lipstick: Cleardown helper
    
    * fix: :bug: Specify `--platform` parameter in `docker build` command for crossplatform build
    
    * Update node/src/tasks/user_sends_file.rs
    
    Co-authored-by: Facundo Farall <37149322+ffarall@users.noreply.github.com>
    
    * fix docker build for mac
    
    * feat: :sparkles: Added check script for bspnet tests
    
    * fix: :bug: Fix Packages
    
    * docs: :memo: Updated README
    
    * added test waits
    
    ---------
    
    Co-authored-by: Facundo Farall <37149322+ffarall@users.noreply.github.com>

---
+++ test/util/bspNet/types.ts
@@ -0,0 +1,56 @@
+import type { ApiPromise } from "@polkadot/api";
+import type { SubmittableExtrinsic } from "@polkadot/api/types";
+import type { KeyringPair } from "@polkadot/keyring/types";
+import type { Codec, IEventData, ISubmittableResult } from "@polkadot/types/types";
+import type { FileSendResponse, SealedBlock } from "./helpers";
+import type { EventRecord, Event } from "@polkadot/types/interfaces";
+
+/**
+ * Represents an enhanced API for interacting with StorageHub BSPNet.
+ */
+export type BspNetApi = ApiPromise & {
+  /**
+   * Seals a block optionally with a given extrinsic and signer.
+   *
+   * @param call - The extrinsic to be included in the block.
+   * @param signer - The keyring pair used to sign the block.
+   * @returns A promise that resolves to a sealed block.
+   */
+  sealBlock: (
+    call?: SubmittableExtrinsic<"promise", ISubmittableResult>,
+    signer?: KeyringPair
+  ) => Promise<SealedBlock>;
+
+  /**
+   * @description Issues a sendFile RPC call to UserNode.
+   * N.B. Local file must exist in mounted volume '/res'
+   *
+   * @param localPath - The local file path.
+   * @param remotePath - The destination path on the blockchain.
+   * @param addressId - The address ID associated with the file transfer.
+   * @returns A promise that resolves to a file send response.
+   */
+  sendFile: (localPath: string, remotePath: string, addressId: string) => Promise<FileSendResponse>;
+
+  /**
+   * @description Asserts that a specific event occurred in a list of events.
+   *
+   * @param module - The module where the event originated.
+   * @param method - The method that triggered the event.
+   * @param events - The list of event records to search through.
+   * @returns An object containing the event and its data.
+   */
+  assertEvent: (
+    module: string,
+    method: string,
+    events?: EventRecord[]
+  ) => { event: Event; data: Codec[] & IEventData };
+
+  /**
+   * @description Fetches an event, inferring its type from the module and method.
+   *
+   * @remarks
+   * This function needs to be implemented.
+   */
+  // fetchEvent: () => void;
+};
---
+++ test/util/timer.ts
@@ -0,0 +1,5 @@
+export const sleep = async (ms: number) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+};
