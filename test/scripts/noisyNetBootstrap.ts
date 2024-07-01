e8029bb44b8ffbfeda6990fbf817ff572a5e7d36
timbrinded <79199034+timbrinded@users.noreply.github.com>
2024-07-01 16:52:26 +0100

    minor fix

--- test/scripts/noisyNetBootstrap.ts
+++ test/scripts/noisyNetBootstrap.ts
@@ -1,4 +1,13 @@
-import { NODE_INFOS, runBspNet, type ToxicInfo } from "../util";
+import {
+  createApiObject,
+  DUMMY_MSP_ID,
+  getContainerPeerId,
+  NODE_INFOS,
+  runBspNet,
+  shUser,
+  type ToxicInfo
+} from "../util";
+import { setTimeout } from "node:timers/promises";
 
 const registerToxic = async (toxicDef: ToxicInfo) => {
   const url = `http://localhost:${NODE_INFOS.toxiproxy.port}/proxies/sh-bsp-proxy/toxics`;
@@ -35,8 +44,8 @@ async function bootStrapNetwork() {
         stream: "downstream",
         toxicity: 1,
         attributes: {
-          latency: 500,
-          jitter: 50
+          latency: 50,
+          jitter: 10
         }
       },
       {
@@ -45,8 +54,8 @@ async function bootStrapNetwork() {
         stream: "upstream",
         toxicity: 1,
         attributes: {
-          latency: 500,
-          jitter: 50
+          latency: 50,
+          jitter: 10
         }
       }
     ] satisfies ToxicInfo[];
@@ -64,12 +73,58 @@ async function bootStrapNetwork() {
       throw new Error("Toxic registration failed");
     }
 
+    // Send file
+    var api = await createApiObject(`ws://127.0.0.1:${NODE_INFOS.user.port}`);
+
+    const bucketName = "nothingmuch-0";
+    const newBucketEventEvent = await api.createBucket(bucketName);
+    const newBucketEventDataBlob =
+      api.events.fileSystem.NewBucket.is(newBucketEventEvent) && newBucketEventEvent.data;
+
+    if (!newBucketEventDataBlob) {
+      throw new Error("Event doesn't match Type");
+    }
+
+    const localPath = "res/whatsup.jpg";
+    const remotePath = "cat/whatsup.jpg";
+
+    // Issue file Storage request
+    const rpcResponse = await api.loadFile(
+      localPath,
+      remotePath,
+      NODE_INFOS.user.AddressId,
+      newBucketEventDataBlob.bucketId
+    );
+    console.log(rpcResponse);
+
+    const peerIDUser = await getContainerPeerId(`http://127.0.0.1:${NODE_INFOS.user.port}`);
+    console.log(`sh-user Peer ID: ${peerIDUser}`);
+
+    await api.sealBlock(
+      api.tx.fileSystem.issueStorageRequest(
+        rpcResponse.bucket_id,
+        remotePath,
+        rpcResponse.fingerprint,
+        rpcResponse.size,
+        DUMMY_MSP_ID,
+        [peerIDUser]
+      ),
+      shUser
+    );
+
+    // Seal the block from BSP volunteer
+    await setTimeout(1000);
+    await api.sealBlock();
+
     console.log("✅ NoisyNet Bootstrap success");
   } catch (e) {
     console.error("Error running bootstrap script:", e);
     console.log("❌ BSPNet Bootstrap failure");
     process.exitCode = 1;
   }
+
+  // @ts-expect-error - bug in tsc
+  await api.disconnect();
 }
 
 bootStrapNetwork();
