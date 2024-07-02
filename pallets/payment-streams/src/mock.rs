01de9fe31975b865e42215b1e53d84f6b94291b6
Tobi Demeco <tdemeco@itba.edu.ar>
2024-07-02 15:58:49 -0300

    style: :rotating_light: fix format

--- pallets/payment-streams/src/mock.rs
+++ pallets/payment-streams/src/mock.rs
@@ -118,7 +118,7 @@ impl pallet_storage_providers::Config for Test {
     type StorageData = StorageUnit;
     type SpCount = u32;
     type MerklePatriciaRoot = H256;
-	type MerkleTrieHashing = BlakeTwo256;
+    type MerkleTrieHashing = BlakeTwo256;
     type ValuePropId = H256;
     type ReadAccessGroupId = <Self as pallet_nfts::Config>::CollectionId;
     type MaxMultiAddressSize = ConstU32<100>;
