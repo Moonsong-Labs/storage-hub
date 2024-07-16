943e246d4acf748caa400996b8e152587b28f487
Murtaza Alexandru <alexandru@moonsonglabs.com>
2024-07-16 18:58:41 +0300

    fix bsp upload race condition (#127)

--- node/src/tasks/user_sends_file.rs
+++ node/src/tasks/user_sends_file.rs
@@ -70,9 +70,6 @@ where
             event.location,
         );
 
-        // TODO: FIND A BETTER WORKAROUND FOR THIS
-        tokio::time::sleep(tokio::time::Duration::from_secs(1)).await;
-
         let file_metadata = FileMetadata {
             owner: <AccountId32 as AsRef<[u8]>>::as_ref(&event.owner).to_vec(),
             bucket_id: event.bucket_id.as_ref().to_vec(),
