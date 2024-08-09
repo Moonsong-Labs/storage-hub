10ab2305c84643adf2b1984998170181a513218f
Alexandru Murtaza <alexandru@moonsonglabs.com>
2024-08-09 17:02:12 +0300

    implement multiple transactions in sealBlock

--- test/suites/integration/bsp/volunteer.test.ts
+++ test/suites/integration/bsp/volunteer.test.ts
@@ -16,6 +16,8 @@ import {
   sleep
 } from "../../../util";
 import { assert } from "node:console";
+import type { SubmittableExtrinsic } from "@polkadot/api/types";
+import type { ISubmittableResult } from "@polkadot/types/types";
 
 const bspNetConfigCases: BspNetConfig[] = [
   { noisy: false, rocksdb: false },
@@ -218,6 +220,85 @@ for (const bspNetConfig of bspNetConfigCases) {
         const sha = await checkFileChecksum("test/whatsup.jpg");
         strictEqual(sha, TEST_ARTEFACTS["res/whatsup.jpg"].checksum);
       });
+    });
+
+    it.only("bsp volunteers multiple files properly", async () => {
+      const source = ["res/whatsup.jpg", "res/adolphus.jpg"];
+      const destination = ["test/whatsup.jpg", "test/adolphus.jpg"];
+      const bucketName = "nothingmuch-3";
+
+      const newBucketEventEvent = await user_api.createBucket(bucketName);
+      const newBucketEventDataBlob =
+        user_api.events.fileSystem.NewBucket.is(newBucketEventEvent) && newBucketEventEvent.data;
+
+      if (!newBucketEventDataBlob) {
+        throw new Error("Event doesn't match Type");
+      }
+
+      let txs: SubmittableExtrinsic<"promise", ISubmittableResult>[] = []
+      for (let i = 0; i < source.length; i++) {
+        const { fingerprint, file_size, location } =
+          await user_api.rpc.storagehubclient.loadFileInStorage(
+            source[i],
+            destination[i],
+            NODE_INFOS.user.AddressId,
+            newBucketEventDataBlob.bucketId
+          );
+
+        txs.push(user_api.tx.fileSystem.issueStorageRequest(
+          newBucketEventDataBlob.bucketId,
+          location,
+          fingerprint,
+          file_size,
+          DUMMY_MSP_ID,
+          [NODE_INFOS.user.expectedPeerId]
+        ));
+      }
+
+      await user_api.sealBlock(
+        txs,
+        shUser
+      );
+
+      await sleep(500); // wait for the bsp to volunteer
+      const volunteer_pending = await bsp_api.rpc.author.pendingExtrinsics();
+      strictEqual(
+        volunteer_pending.length,
+        source.length,
+        "There should be pending extrinsics for all files from BSP (volunteer)"
+      );
+
+      await bsp_api.sealBlock();
+      const [resBspId, resBucketId, resLoc, resFinger, resMulti, _, resSize] = fetchEventData(
+        bsp_api.events.fileSystem.AcceptedBspVolunteer,
+        await bsp_api.query.system.events()
+      );
+
+      await sleep(5000); // wait for the bsp to download the file
+      const confirm_pending = await bsp_api.rpc.author.pendingExtrinsics();
+      strictEqual(
+        confirm_pending.length,
+        1,
+        "There should be one pending extrinsic from BSP (confirm store)"
+      );
+
+      await bsp_api.sealBlock();
+      const [
+        _bspConfirmRes_who,
+        bspConfirmRes_bspId,
+        bspConfirmRes_fileKeys,
+        bspConfirmRes_newRoot
+      ] = fetchEventData(
+        bsp_api.events.fileSystem.BspConfirmedStoring,
+        await bsp_api.query.system.events()
+      );
+
+      strictEqual(bspConfirmRes_fileKeys.length, 1);
+
+
+      await sleep(1000); // wait for the bsp to process the BspConfirmedStoring event
+      const bsp_forest_root_after_confirm = await bsp_api.rpc.storagehubclient.getForestRoot();
+      strictEqual(bsp_forest_root_after_confirm.toString(), bspConfirmRes_newRoot.toString());
     });
   });
 }
