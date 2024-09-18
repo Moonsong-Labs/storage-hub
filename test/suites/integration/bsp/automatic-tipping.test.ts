89f61bb1b30d51f6a2d45929cd9fccebf808dd99
Alexandru Murtaza <alexandru@moonsonglabs.com>
2024-09-19 01:50:16 +0300

    update integration test with new API

--- test/suites/integration/bsp/automatic-tipping.test.ts
+++ test/suites/integration/bsp/automatic-tipping.test.ts
@@ -1,73 +1,48 @@
-import { strictEqual } from "node:assert";
-import { describeBspNet, shUser, sleep, type EnrichedBspApi } from "../../../util";
+import { describeBspNet, sleep, type EnrichedBspApi } from "../../../util";
 import { assert } from "node:console";
 
 describeBspNet(
   "BSP Automatic Tipping",
   { extrinsicRetryTimeout: 2 },
   ({ before, it, createUserApi }) => {
-    let userApi: EnrichedBspApi;
+    let api: EnrichedBspApi;
 
     before(async () => {
-      userApi = await createUserApi();
+      api = await createUserApi();
     });
 
     it("Confirm storing failure results in increased tip", async () => {
-      const source = "res/whatsup.jpg";
-      const destination = "test/whatsup.jpg";
-      const bucketName = "nothingmuch-2";
-
-      const newBucketEventEvent = await userApi.createBucket(bucketName);
-      const newBucketEventDataBlob =
-        userApi.events.fileSystem.NewBucket.is(newBucketEventEvent) && newBucketEventEvent.data;
-
-      if (!newBucketEventDataBlob) {
-        throw new Error("Event doesn't match Type");
-      }
-
-      const { fingerprint, file_size, location } =
-        await userApi.rpc.storagehubclient.loadFileInStorage(
-          source,
-          destination,
-          userApi.shConsts.NODE_INFOS.user.AddressId,
-          newBucketEventDataBlob.bucketId
-        );
-
-      await userApi.sealBlock(
-        userApi.tx.fileSystem.issueStorageRequest(
-          newBucketEventDataBlob.bucketId,
-          location,
-          fingerprint,
-          file_size,
-          userApi.shConsts.DUMMY_MSP_ID,
-          [userApi.shConsts.NODE_INFOS.user.expectedPeerId]
-        ),
-        shUser
-      );
-
-      await sleep(500); // wait for the bsp to volunteer
-      await userApi.sealBlock();
+      await api.file.newStorageRequest("res/whatsup.jpg", "test/whatsup.jpg", "nothingmuch-2");
+      await api.wait.bspVolunteer();
 
       await sleep(1000); // wait for the bsp to download the files
 
-      const confirmPending = await userApi.rpc.author.pendingExtrinsics();
-      strictEqual(
-        confirmPending.length,
-        1,
-        "There should be one pending extrinsic from BSP (confirm store)"
+      await api.assert.extrinsicPresent({
+        method: "bspConfirmStoring",
+        module: "fileSystem",
+        checkTxPool: true,
+        assertLength: 1
+      });
+
+      await sleep(6000); // wait for the bsp to send all confirm retries
+
+      const confirmStoringPendingMatches = await api.assert.extrinsicPresent({
+        method: "bspConfirmStoring",
+        module: "fileSystem",
+        checkTxPool: true,
+        assertLength: 4
+      });
+
+      const txPool = await api.rpc.author.pendingExtrinsics();
+
+      let confirmStoringPendingExts = confirmStoringPendingMatches.map(
+        (match) => txPool[match.extIndex]
       );
 
-      await sleep(6000); // wait for the bsp to send confirm again
-      const confirmPending2 = await userApi.rpc.author.pendingExtrinsics();
-      const expectedRetryCount = 4;
-      strictEqual(
-        confirmPending2.length,
-        expectedRetryCount,
-        "There should be 4 pending extrinsic from BSP (confirm store) with increasing tip"
-      );
-      for (let i = 1; i < confirmPending2.length; ++i) {
+      for (let i = 1; i < confirmStoringPendingExts.length; ++i) {
         assert(
-          confirmPending2[i].tip > confirmPending2[i - 1].tip,
+          confirmStoringPendingExts[i].tip.toBigInt() >
+            confirmStoringPendingExts[i - 1].tip.toBigInt(),
           "Tip should increase with each retry"
         );
       }
