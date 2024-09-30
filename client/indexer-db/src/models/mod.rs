a718f13b7c381500ac05b54d78f657e768b86f44
Alexandru Murtaza <alexandru@moonsonglabs.com>
2024-09-30 15:30:20 +0300

    fix

--- client/indexer-db/src/models/mod.rs
+++ client/indexer-db/src/models/mod.rs
@@ -1,13 +1,11 @@
 pub mod bsp;
 pub mod bucket;
-pub mod file;
 pub mod msp;
 pub mod multiaddress;
 pub mod service_state;
 
 pub use bsp::*;
 pub use bucket::*;
-// pub use file::*;
 pub use msp::*;
 pub use multiaddress::*;
 pub use service_state::*;
