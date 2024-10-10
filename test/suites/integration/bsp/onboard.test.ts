cf60ca1ab774e0d1bf82e48673c6925620e2604a
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2024-10-10 13:28:34 -0300

    fix: :rotating_light: Remove unused `sleep` import

--- test/suites/integration/bsp/onboard.test.ts
+++ test/suites/integration/bsp/onboard.test.ts
@@ -1,12 +1,6 @@
 import Docker from "dockerode";
 import assert, { strictEqual } from "node:assert";
-import {
-  addBspContainer,
-  describeBspNet,
-  DOCKER_IMAGE,
-  sleep,
-  type EnrichedBspApi
-} from "../../../util";
+import { addBspContainer, describeBspNet, DOCKER_IMAGE, type EnrichedBspApi } from "../../../util";
 import { CAPACITY, MAX_STORAGE_CAPACITY } from "../../../util/bspNet/consts.ts";
 
 describeBspNet("BSPNet: Adding new BSPs", ({ before, createBspApi, createApi, it }) => {
