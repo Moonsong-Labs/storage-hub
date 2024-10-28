7c42d80fa25ac6e381b2a36f8597b96e75273571
Tobi Demeco <50408393+TDemeco@users.noreply.github.com>
2024-10-28 20:19:03 -0300

    feat: :sparkles: add batching to user charging (#229)
    * feat: :sparkles: add batching to user charging
    
    * chore: :label: run typegen
    
    * chore: :label: run typegen
    
    * test: :white_check_mark: add extra test scenarios
    
    * style: :rotating_light: run cargo fmt

--- runtime/src/configs/mod.rs
+++ runtime/src/configs/mod.rs
@@ -518,6 +518,7 @@ impl pallet_payment_streams::Config for Runtime {
     type Units = StorageDataUnit; // Storage unit
     type BlockNumberToBalance = BlockNumberToBalance;
     type ProvidersProofSubmitters = ProofsDealer;
+    type MaxUsersToCharge = ConstU32<10>;
 }
 
 // TODO: remove this and replace with pallet treasury
