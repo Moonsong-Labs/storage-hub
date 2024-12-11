45626e387ccaf785c326231cc9160f7d83b34f57
TDemeco <tdemeco@itba.edu.ar>
2024-12-10 23:08:36 -0300

    chore: :rotating_light: run cargo fmt

--- pallets/payment-streams/src/lib.rs
+++ pallets/payment-streams/src/lib.rs
@@ -200,8 +200,8 @@ pub mod pallet {
     ///
     /// This storage is updated in:
     /// - [charge_payment_streams](crate::dispatchables::charge_payment_streams), which emits a `UserWithoutFunds` event and sets the user's entry in this map
-	/// to that moment's tick number if it does not have enough funds.
-	/// - [clear_insolvent_flag](crate::utils::clear_insolvent_flag), which clears the user's entry in this map if the cooldown period has passed and the user has paid all its outstanding debt.
+    /// to that moment's tick number if it does not have enough funds.
+    /// - [clear_insolvent_flag](crate::utils::clear_insolvent_flag), which clears the user's entry in this map if the cooldown period has passed and the user has paid all its outstanding debt.
     #[pallet::storage]
     pub type UsersWithoutFunds<T: Config> =
         StorageMap<_, Blake2_128Concat, T::AccountId, BlockNumberFor<T>>;
