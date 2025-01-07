17cfc91e19a05521199c74e42870cbb912015b44
undercover-cactus <lola@moonsonglabs.com>
2025-01-07 14:49:24 +0100

    wip

--- client/common/src/config.rs
+++ client/common/src/config.rs
@@ -4,7 +4,17 @@ use std::io::prelude::*;
 use toml;
 
 #[derive(Clone, Debug, Deserialize)]
-pub struct Config {}
+pub struct Config {
+    dev: bool,
+    provider: bool,
+    provider_type: String,
+    max_storage_capacity: u64, // Is this supposed to be big number ?
+    jump_capacity: u64,        // Same question here
+    name: String,
+    no_hardware_benchmarks: bool,
+    unsafe_rpc_external: bool,
+    rpc_methods: bool,
+}
 
 pub fn read_config() -> Config {
     let mut file = File::open("config.toml").expect("config.toml file required");
--- client/common/src/lib.rs
+++ client/common/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod blockchain_utils;
+pub mod config;
 pub mod consts;
 pub mod types;
