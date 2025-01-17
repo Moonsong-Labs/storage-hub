0b60b0e8c800a4e3f943c73e37abd5464c2a005c
Tim B <79199034+timbrinded@users.noreply.github.com>
2025-01-17 13:21:24 +0000

    refactor: :recycle: Poll for proof submission ext

--- test/suites/integration/bsp/challenge-cycle.test.ts
+++ test/suites/integration/bsp/challenge-cycle.test.ts
@@ -50,7 +50,11 @@ describeBspNet(
 
       // Wait for task to execute and seal one more block.
       // In this block, the BSP should have submitted a proof.
-      await sleep(500);
+      await userApi.assert.extrinsicPresent({
+        module: "proofsDealer",
+        method: "submitProof",
+        checkTxPool: true
+      });
       await userApi.block.seal();
 
       // Assert for the the event of the proof successfully submitted and verified.
@@ -143,7 +147,11 @@ describeBspNet(
 
         // Wait for task to execute and seal one more block.
         // In this block, the BSP should have submitted a proof.
-        await sleep(500);
+        await userApi.assert.extrinsicPresent({
+          module: "proofsDealer",
+          method: "submitProof",
+          checkTxPool: true
+        });
         await userApi.block.seal();
 
         // Assert for the the event of the proof successfully submitted and verified.
