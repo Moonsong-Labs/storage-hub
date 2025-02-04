62f090e728f93624799e6b9ebae8f8a9966ebaf7
Michael Assaf <michael.assaf.edge@gmail.com>
2025-02-04 14:22:29 -0500

    fix benchmark parameters

--- pallets/file-system/src/benchmarking.rs
+++ pallets/file-system/src/benchmarking.rs
@@ -1870,7 +1870,8 @@ mod benchmarks {
                     file_key: Default::default(),
                     bucket_id: Default::default(),
                     file_size: i.into(),
-					deposit_paid_for_creation: file_deletion_request_deposit,
+                    deposit_paid_for_creation: file_deletion_request_deposit,
+                    queue_priority_challenge: true
                 })
                 .unwrap_or_else(|_| panic!("Should be able to push to the BoundedVec since range is smaller than its size"));
         }
@@ -2174,7 +2175,8 @@ mod benchmarks {
                     file_key: Default::default(),
                     bucket_id: Default::default(),
                     file_size: i.into(),
-					deposit_paid_for_creation: file_deletion_request_deposit,
+           					deposit_paid_for_creation: file_deletion_request_deposit,
+           					queue_priority_challenge: true
                 })
                 .unwrap_or_else(|_| panic!("Should be able to push to the BoundedVec since range is smaller than its size"));
         }
