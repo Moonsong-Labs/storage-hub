1efaae1ec055975c7db71abf935f9c021d91939c
TDemeco <tdemeco@itba.edu.ar>
2025-02-25 10:46:44 -0300

    test: :fire: remove `only` from MSP stop storing bucket for insolvent user integration test

--- test/suites/integration/msp/stop-storing-bucket-insolvent-user.test.ts
+++ test/suites/integration/msp/stop-storing-bucket-insolvent-user.test.ts
@@ -4,7 +4,6 @@ import { MSP_CHARGING_PERIOD } from "../../../util/bspNet/consts";
 
 describeMspNet(
   "MSP test: MSP stops storing buckets that belong to insolvent users",
-  { only: true },
   ({ before, createMsp1Api, it, createUserApi }) => {
     let userApi: EnrichedBspApi;
     let mspApi: EnrichedBspApi;
