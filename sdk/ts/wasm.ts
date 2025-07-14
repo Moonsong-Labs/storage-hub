043c5378820396da5668cb38ffcafa258be802d1
ftheirs <fntheirs@gmail.com>
2025-07-14 15:03:11 -0300

    format

--- sdk/ts/wasm.ts
+++ sdk/ts/wasm.ts
@@ -3,4 +3,4 @@ const require = createRequire(import.meta.url);
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
 const wasm = require('@storagehub/wasm') as typeof import('../pkg/storagehub_wasm');
 
-export const { add } = wasm; 
+export const { add } = wasm;
