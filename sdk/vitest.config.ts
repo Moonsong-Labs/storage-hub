e84f3c0cb0c18e5a16494252b0832f9ed183ecf3
ftheirs <fntheirs@gmail.com>
2025-07-14 17:23:50 -0300

    coverage script & ci

--- sdk/vitest.config.ts
+++ sdk/vitest.config.ts
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vitest/config';
 
+// TODO: set coverage to 95% according to specs
+const COVERAGE_THRESHOLD = 0;
+
 export default defineConfig({
     test: {
         environment: 'node',
         globals: true,
         coverage: {
             provider: 'v8',
+            exclude: ['scripts/**', 'wasm/pkg/**'],
+            reporter: ['text', 'html'],
+            all: true,
+            thresholds: {
+                statements: COVERAGE_THRESHOLD,
+                branches: COVERAGE_THRESHOLD,
+                functions: COVERAGE_THRESHOLD,
+                lines: COVERAGE_THRESHOLD,
+            },
         },
     },
 });
