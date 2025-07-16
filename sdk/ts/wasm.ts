91afd02ff964fa95a5ca9dc3c160911c71d519ef
ftheirs <fntheirs@gmail.com>
2025-07-16 09:36:02 -0300

    Update sdk/ts/wasm.ts
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- sdk/ts/wasm.ts
+++ sdk/ts/wasm.ts
@@ -1,6 +1,6 @@
 import { createRequire } from 'node:module';
 const require = createRequire(import.meta.url);
 // eslint-disable-next-line @typescript-eslint/consistent-type-imports
-const wasm = require('../wasm/pkg') as typeof import('../wasm/pkg/storagehub_wasm.js');
+import * as wasm from '../wasm/pkg';
 
 export const { add } = wasm;
