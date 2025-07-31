3889047d8d4eafea81f399b4fca116eeaee8b92c
ftheirs <fntheirs@gmail.com>
2025-07-31 14:45:29 -0300

    export new classes

--- sdk/core/src/index.ts
+++ sdk/core/src/index.ts
@@ -1,1 +1,4 @@
-export { FileManager } from './file-manager';
+export { WalletBase } from './wallet/base.js';
+export { LocalWallet } from './wallet/local.js';
+export { MetamaskWallet } from './wallet/metamask.js';
+export { FileManager } from './file-manager.js';
