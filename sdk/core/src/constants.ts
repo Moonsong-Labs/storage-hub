2d98638aa06d07238a503ee2510564993affd20f
ftheirs <fntheirs@gmail.com>
2025-08-01 15:52:04 -0300

    feat: SDK Filemanager (#425)
    * add FileManager related classes to SDK wasm
    
    * FileManager TS class
    
    * FileTrie and FileKey TS tests
    
    * project updates
    
    * fix CI
    
    * make FileManager environment agnostic
    
    * clarification about CHUNK_SIZE constant
    
    * remove unnecessary check
    
    * avoid exporting internal SDK types
    
    * update comments
    
    * remove assert
    
    * better error handling
    
    * update tests to new FileManager structure
    
    * update pnpm-lock
    
    * fix typo
    
    * add TODO for future refactor
    
    * feedback from PR review
    
    * move include to test section
    
    * missing newline at the end
    
    ---------
    
    Co-authored-by: Tobi Demeco <50408393+TDemeco@users.noreply.github.com>

---
+++ sdk/core/src/constants.ts
@@ -0,0 +1,4 @@
+/// The file chunk size in bytes. This is the size of the leaf nodes in the Merkle
+/// Patricia Trie that is constructed for each file.
+/// Each chunk is 1 kB.
+export const CHUNK_SIZE = 1024;
