3cd6467c9246e1a87861bd5cf22791f727cd8fec
ftheirs <fntheirs@gmail.com>
2025-08-28 11:30:53 -0300

    lint-format

--- sdk/core/src/index.ts
+++ sdk/core/src/index.ts
@@ -1,6 +1,6 @@
 export { FileManager } from './file-manager';
-export { HttpClient } from './http/HttpClient';
 export type { HttpClientConfig } from './http/HttpClient';
+export { HttpClient } from './http/HttpClient';
 export { WalletBase } from './wallet/base';
 export { Eip1193Wallet } from './wallet/eip1193';
 export { WalletError, type WalletErrorCode } from './wallet/errors';
