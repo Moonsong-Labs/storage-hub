21a03c6c0b00f6ab18e4b339efea4214d3600797
ftheirs <fntheirs@gmail.com>
2025-09-04 16:55:06 -0300

    nextjs example

--- sdk/examples/nextjs/app/page.tsx
+++ sdk/examples/nextjs/app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import { Test } from "../src/Test";
 
 export default function Home() {
   return (
@@ -50,6 +51,8 @@ export default function Home() {
             Read our docs
           </a>
         </div>
+
+        <Test />
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
         <a
