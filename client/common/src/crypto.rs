ecfdf9c90725d25ee9f3cd4b4a4fd13197d68d93
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2025-09-12 02:32:53 -0300

    fix: :bug: Properly derive eth address from compressed account ID

--- client/common/src/crypto.rs
+++ client/common/src/crypto.rs
@@ -5,7 +5,9 @@
 
 use codec::{Decode, Encode};
 use fp_account::{AccountId20, EthereumSignature};
-use sp_core::{ecdsa, keccak_256, sr25519, H160, H256};
+use k256::elliptic_curve::sec1::ToEncodedPoint;
+use k256::PublicKey as K256PublicKey;
+use sp_core::{blake2_256, ecdsa, keccak_256, sr25519, H160, H256};
 use sp_keystore::{Keystore, KeystorePtr};
 use sp_runtime::{
     traits::{IdentifyAccount, Verify},
@@ -72,8 +74,18 @@ impl KeyTypeOperations for EthereumSignature {
         public: &Self::Public,
         msg: &[u8],
     ) -> Option<Self> {
+        let hashed_msg: H256;
+        let msg = if msg.len() > 256 {
+            hashed_msg = H256::from(blake2_256(msg));
+            hashed_msg.as_ref()
+        } else {
+            msg
+        };
+
+        let hashed_msg = H256::from(keccak_256(msg));
+
         keystore
-            .ecdsa_sign(key_type, public, msg)
+            .ecdsa_sign(key_type, public, hashed_msg.as_ref())
             .ok()
             .flatten()
             .map(|ecdsa_sig| EthereumSignature::new(ecdsa_sig))
@@ -95,6 +107,14 @@ impl KeyTypeOperations for EthereumSignature {
     }
 
     fn public_to_address(public: &Self::Public) -> Self::Address {
-        AccountId20(H160::from(H256::from(keccak_256(&public))).0)
+        // Ethereum address is last 20 bytes of Keccak-256 of the uncompressed secp256k1 public key (without the 0x04 prefix)
+        // `ecdsa::Public` is a 33-byte compressed SEC1 key; decompress before hashing
+        let sec1_compressed_bytes: &[u8] = public.as_ref();
+        let k256_pubkey = K256PublicKey::from_sec1_bytes(sec1_compressed_bytes)
+            .expect("Compressed secp256k1 public key from keystore should be valid; qed");
+        let uncompressed = k256_pubkey.to_encoded_point(false);
+        let uncompressed_bytes = uncompressed.as_bytes(); // 65 bytes, first is 0x04
+        let hash = keccak_256(&uncompressed_bytes[1..]); // drop 0x04 prefix
+        AccountId20(H160::from(H256::from(hash)).0)
     }
 }
