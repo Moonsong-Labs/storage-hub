a8db4ccd60ecd98ce0e176a5f0126e67e3987cae
ftheirs <fntheirs@gmail.com>
2025-10-02 13:47:45 -0300

    info module

--- sdk/msp-client/src/MspClient.ts
+++ sdk/msp-client/src/MspClient.ts
@@ -1,13 +1,4 @@
-import type {
-  Bucket,
-  FileInfo,
-  HealthStatus,
-  InfoResponse,
-  StatsResponse,
-  UploadOptions,
-  UploadReceipt,
-  ValueProp
-} from "./types.js";
+import type { Bucket, FileInfo, UploadOptions, UploadReceipt } from "./types.js";
 import type { HttpClientConfig } from "@storagehub-sdk/core";
 import { FileMetadata, FileTrie, HttpClient, initWasm } from "@storagehub-sdk/core";
 import type { MspClientContext } from "./context.js";
@@ -15,6 +6,7 @@ import { AuthModule } from "./modules/auth.js";
 import { BucketsModule } from "./modules/buckets.js";
 import { ModuleBase } from "./base.js";
 import { FilesModule } from "./modules/files.js";
+import { InfoModule } from "./modules/info.js";
 
 export class MspClient extends ModuleBase {
   public readonly config: HttpClientConfig;
@@ -23,6 +15,7 @@ export class MspClient extends ModuleBase {
   public readonly auth: AuthModule;
   public readonly buckets: BucketsModule;
   public readonly files: FilesModule;
+  public readonly info: InfoModule;
 
   private constructor(config: HttpClientConfig, http: HttpClient) {
     const context: MspClientContext = { config, http };
@@ -33,6 +26,7 @@ export class MspClient extends ModuleBase {
     this.auth = new AuthModule(this.context);
     this.buckets = new BucketsModule(this.context);
     this.files = new FilesModule(this.context);
+    this.info = new InfoModule(this.context);
   }
 
   static async connect(config: HttpClientConfig): Promise<MspClient> {
@@ -48,33 +42,6 @@ export class MspClient extends ModuleBase {
     });
 
     return new MspClient(config, http);
-  }
-
-  getHealth(options?: { signal?: AbortSignal }): Promise<HealthStatus> {
-    return this.http.get<HealthStatus>("/health", {
-      ...(options?.signal !== undefined && { signal: options.signal })
-    });
-  }
-
-  /** Get general MSP information */
-  getInfo(options?: { signal?: AbortSignal }): Promise<InfoResponse> {
-    return this.http.get<InfoResponse>("/info", {
-      ...(options?.signal !== undefined && { signal: options.signal })
-    });
-  }
-
-  /** Get MSP statistics */
-  getStats(options?: { signal?: AbortSignal }): Promise<StatsResponse> {
-    return this.http.get<StatsResponse>("/stats", {
-      ...(options?.signal !== undefined && { signal: options.signal })
-    });
-  }
-
-  /** Get available value propositions */
-  getValuePropositions(options?: { signal?: AbortSignal }): Promise<ValueProp[]> {
-    return this.http.get<ValueProp[]>("/value-props", {
-      ...(options?.signal !== undefined && { signal: options.signal })
-    });
   }
 
 }
--- sdk/msp-client/src/modules/info.ts
+++ sdk/msp-client/src/modules/info.ts
@@ -1,9 +1,37 @@
 import type { MspClientContext } from "../context.js";
 import { ModuleBase } from "../base.js";
+import type { HealthStatus, InfoResponse, StatsResponse, ValueProp } from "../types.js";
 
 export class InfoModule extends ModuleBase {
   constructor(ctx: MspClientContext) {
     super(ctx);
+  }
+
+  getHealth(options?: { signal?: AbortSignal }): Promise<HealthStatus> {
+    return this.ctx.http.get<HealthStatus>("/health", {
+      ...(options?.signal !== undefined && { signal: options.signal })
+    });
+  }
+
+  /** Get general MSP information */
+  getInfo(options?: { signal?: AbortSignal }): Promise<InfoResponse> {
+    return this.ctx.http.get<InfoResponse>("/info", {
+      ...(options?.signal !== undefined && { signal: options.signal })
+    });
+  }
+
+  /** Get MSP statistics */
+  getStats(options?: { signal?: AbortSignal }): Promise<StatsResponse> {
+    return this.ctx.http.get<StatsResponse>("/stats", {
+      ...(options?.signal !== undefined && { signal: options.signal })
+    });
+  }
+
+  /** Get available value propositions */
+  getValuePropositions(options?: { signal?: AbortSignal }): Promise<ValueProp[]> {
+    return this.ctx.http.get<ValueProp[]>("/value-props", {
+      ...(options?.signal !== undefined && { signal: options.signal })
+    });
   }
 }
 
