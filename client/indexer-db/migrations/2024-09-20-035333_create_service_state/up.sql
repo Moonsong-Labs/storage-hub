dd2c2d355996976e606c01dd1d36e5f67b5f9972
Michael Assaf <94772640+snowmead@users.noreply.github.com>
2025-10-06 16:20:14 -0400

    style: rename `last_indexed_block` to `last_indexed_finalized_block` (#501)

--- client/indexer-db/migrations/2024-09-20-035333_create_service_state/up.sql
+++ client/indexer-db/migrations/2024-09-20-035333_create_service_state/up.sql
@@ -1,8 +1,8 @@
 CREATE TABLE service_state (
     id INT PRIMARY KEY CHECK (id = 1),                              -- Enforce only one row
-    last_processed_block BIGINT NOT NULL,
+    last_indexed_finalized_block BIGINT NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-INSERT INTO service_state (id, last_processed_block) VALUES (1, 0);
+INSERT INTO service_state (id, last_indexed_finalized_block) VALUES (1, 0);
