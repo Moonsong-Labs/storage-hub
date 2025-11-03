6f8ffdca0912ddb8f6c23990bd9ac443db3d3eed
Herman Obst Demaestri <hodemaestri@gmail.com>
2025-11-03 15:12:45 -0300

    fix: msp_delete_task receives the StorageRequestRejected event

--- client/src/handler.rs
+++ client/src/handler.rs
@@ -15,9 +15,10 @@ use shc_blockchain_service::{
         AcceptedBspVolunteer, DistributeFileToBsp, FinalisedBspConfirmStoppedStoring,
         FinalisedBucketMovedAway, FinalisedBucketMutationsApplied,
         FinalisedMspStopStoringBucketInsolventUser, FinalisedMspStoppedStoringBucket,
-        FinalisedTrieRemoveMutationsAppliedForBsp, LastChargeableInfoUpdated, MoveBucketAccepted,
-        MoveBucketExpired, MoveBucketRejected, MoveBucketRequested, MoveBucketRequestedForMsp,
-        MultipleNewChallengeSeeds, NewStorageRequest, NotifyPeriod, ProcessConfirmStoringRequest,
+        FinalisedStorageRequestExpired, FinalisedTrieRemoveMutationsAppliedForBsp,
+        LastChargeableInfoUpdated, MoveBucketAccepted, MoveBucketExpired, MoveBucketRejected,
+        MoveBucketRequested, MoveBucketRequestedForMsp, MultipleNewChallengeSeeds,
+        NewStorageRequest, NotifyPeriod, ProcessConfirmStoringRequest,
         ProcessMspRespondStoringRequest, ProcessStopStoringForInsolventUserRequest,
         ProcessSubmitProofRequest, SlashableProvider, SpStopStoringInsolventUser,
         StartMovedBucketDownload, UserWithoutFunds, VerifyMspBucketForests,
@@ -319,6 +320,7 @@ where
                 DistributeFileToBsp<Runtime> => MspDistributeFileTask,
                 // MspRemoveFinalisedFilesTask handles events for removing files from file storage after mutations are finalised.
                 FinalisedBucketMutationsApplied<Runtime> => MspDeleteFileTask,
+                FinalisedStorageRequestExpired<Runtime> => MspDeleteFileTask,
                 VerifyMspBucketForests => MspVerifyBucketForestsTask,
             ]
         );
