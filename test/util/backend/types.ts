88a32294343fb1e81105fce2fae6ffe4337f545b
santikaplan <121105110+santikaplan@users.noreply.github.com>
2026-02-25 17:12:14 -0300

    feat: Add desiredReplicas and currentReplication to SDK FileInfo types

--- test/util/backend/types.ts
+++ test/util/backend/types.ts
@@ -63,6 +63,8 @@ export interface FileInfo {
   isPublic: boolean;
   uploadedAt: string;
   status: FileStatus;
+  desiredReplicas: number;
+  currentReplication: number;
 }
 
 export interface PaymentStream {
