3e6a1fa3c98bd2ca1636555e41628bd003132fec
Facundo Farall <37149322+ffarall@users.noreply.github.com>
2026-03-12 15:43:18 -0300

    revert: :rewind: Revert unnecessary change

--- backend/lib/src/data/rpc/client.rs
+++ backend/lib/src/data/rpc/client.rs
@@ -97,7 +97,8 @@ impl StorageHubRpcClient {
         // The RPC also replies with a hex-encoded SCALE-encoded response
         let response = hex::decode(response.trim_start_matches("0x")).map_err(|e| {
             RpcConnectionError::Serialization(format!(
-                "RPC runtime API did not respond with a valid hex string: {e}",
+                "RPC runtime API did not respond with a valid hex string: {}",
+                e
             ))
         })?;
 
@@ -135,7 +136,8 @@ impl StorageHubRpcClient {
         // The RPC replies with a hex-encoded SCALE-encoded response
         let response = hex::decode(response.trim_start_matches("0x")).map_err(|e| {
             RpcConnectionError::Serialization(format!(
-                "RPC runtime API did not respond with a valid hex string: {e}",
+                "RPC runtime API did not respond with a valid hex string: {}",
+                e
             ))
         })?;
 
